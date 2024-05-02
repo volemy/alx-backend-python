c00f9764af587fa30f36d0f4470364b18069e00e
volemy <minayobenevolence0@gamil.com>
2024-05-02 17:35:00 +0300

    task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin env python3
+""" This module contains the floor function that takes n as an argument
+"""
+
+from math import floor as _floor
+
+
+def floor(n: float) -> int:
+    """
+    This function returns the floor of a float number.
+    """
+    return _floor(n)
