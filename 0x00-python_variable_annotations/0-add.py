9f44a093839feda60b8fd4fd3f8e2b13f1cd85d9
volemy <minayobenevolence0@gamil.com>
2024-05-02 17:16:44 +0300

    Task 1/Add function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This module contains a type-annotated function
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    This method returns the sum of two float number.
+    Returns float: sum of input numbers
+    """
+    return a + b
