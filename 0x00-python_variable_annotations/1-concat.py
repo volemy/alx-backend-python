a54a221c2cfc4f0891309445234f210e53d4c3ac
volemy <minayobenevolence0@gamil.com>
2024-05-02 17:27:13 +0300

    task2/concat function

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This method contains a concat function.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    This function returns the concatenation of two strings
+    """
+    return str1 + str2
