23bb96dedddaef21bba93c6cbf93e0f08369e271
volemy <minayobenevolence0@gamil.com>
2024-05-02 22:02:03 +0300

    task 10

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -3,10 +3,10 @@
 Augmenting code with the correct duck-typed annotations
 """
 
-from typing import Any, Sequence, Union, Optional
+from typing import Sequence, Any, Union, Type
 
 
-def safe_first_element(lst: sequence[Any]) -> Optional[Any]:
+def safe_first_element(lst: sequence[Any]) -> Union[Any, Type[None]]:
     """
     This returns the first element of a sequnence or None if sequence is empty
     """
