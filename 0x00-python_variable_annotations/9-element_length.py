ac2b90adf45749aadf626c9c78802c668f557a44
volemy <minayobenevolence0@gamil.com>
2024-05-02 19:00:27 +0300

    task 9

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This module adds annotations to below function
+"""
+
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(Lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    This function returns a list of tuples where each tuple contains a 
+    sequence and an integer
+    """
+    return [(i, len(i)) for i in Lst]
