7e460b67259d282547a921b25d09d53212ddb662
volemy <minayobenevolence0@gamil.com>
2024-05-06 17:06:40 +0300

    task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+This module includes the wait_n coroutine that manages multiple instances of
+wait_random function.
+"""
+
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    This method spawns wait_random n times with the specified max_delay.
+    """
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    results = []
+    for task in asyncio.as_completed(tasks):
+        result = await task
+        results.append(result)
+    return results
