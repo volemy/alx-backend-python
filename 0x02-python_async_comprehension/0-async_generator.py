891e2f36b372d5d0fabee940bc79812fef586e9e
volemy <minayobenevolence0@gamil.com>
2024-05-07 21:13:11 +0300

    task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,9 +6,10 @@ takes no arguments
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator() -> asyncio.StreamReader:
+async def async_generator() -> Generator[float, None, None]:
     """
     An asynchronous generator that yields random numbers between 0 and 10
     with a 1-second delay between each yield.
